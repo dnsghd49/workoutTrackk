2bc27afc63eed2a3b5c4e5945c6ff2fff15a0e77
Aaron Lu <aaronnoh0409@gmail.com>
2021-01-29 00:06:15 -0500

    created new server and deploying to heroku

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workout',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
+  process.env.MONGODB_URI || 'mongodb://localhost/workoutTracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 
 require("./routes/apiRoutes.js")(app);
