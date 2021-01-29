616454af1e7436ffca731abe1e3672fd7c8ea4be
Aaron Lu <aaronnoh0409@gmail.com>
2021-01-29 00:12:14 -0500

    this should work now

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb://localhost/workoutTrack", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
