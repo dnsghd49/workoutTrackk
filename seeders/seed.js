71f8f09caee45e8ea2782e2595c8a04d7e909a6a
Aaron Lu <aaronnoh0409@gmail.com>
2021-01-29 00:16:03 -0500

    fixing little stuffs

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workoutTrack", {
+mongoose.connect("mongodb://localhost/workoutTracker", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
@@ -124,8 +124,8 @@ let workoutSeed = [
   }
 ];
 
-db.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
+db.WorkoutTracker.deleteMany({})
+  .then(() => db.WorkoutTracker.collection.insertMany(workoutSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
