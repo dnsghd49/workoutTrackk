2ebcae3d3659c3b6bc7135ac0ccd6d07b51edee7
Aaron Lu <aaronnoh0409@gmail.com>
2021-01-28 22:30:58 -0500

    modified server.js

--- server.js
+++ server.js
@@ -1,3 +1,38 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+
+require("./routes/apiRoutes.js")(app);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
